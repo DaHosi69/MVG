7af375284cd5a6de64ad09c98b68afd2ddec701c
DaHosi <hosnernoah5@gmail.com>
2025-01-05 17:52:21 +0100

    404 page added and navbar changes in reservations

--- Frontend/Sitzplatzverwaltung_Frontend/src/app/app.routes.ts
+++ Frontend/Sitzplatzverwaltung_Frontend/src/app/app.routes.ts
@@ -4,11 +4,12 @@ import {OverviewComponent} from "./components/overview-page/overview/overview.co
 import { ReservationsComponent } from './components/reservation-page/reservations/reservations.component';
 import { reservationsResolver } from './components/reservation-page/resolvers/reservations.resolver';
 import { AuthGuard } from '../services/auth.guard';
+import { WrongPathComponent } from './components/wrong-path/wrong-path.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'overview', component: OverviewComponent, canActivate: [AuthGuard]},
   {path: 'reservations/:id', component: ReservationsComponent, resolve: { seats: reservationsResolver }, canActivate: [AuthGuard] },
-  {path: '**', redirectTo: 'login'}
+  {path: '**', component: WrongPathComponent}
 ];
